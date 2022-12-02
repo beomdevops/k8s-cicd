94948930d78d117a1d51a8a0995cd54dfe4b7c0e
beomdevops <beomsun66@gmail.com>
2022-12-02 10:35:42 +0900

    helloservice

---
+++ src/testservicev1/src/main/java/com/example/testservicev1/Testservicev1Application.java
@@ -0,0 +1,13 @@
+package com.example.testservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testservicev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testservicev1Application.class, args);
+	}
+
+}
